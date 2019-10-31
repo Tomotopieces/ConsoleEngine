6fa8fb2043fcb49a435c6fdb0335ca39194c184c
Tomotopieces <46648918+Tomotopieces@users.noreply.github.com>
2019-10-31 21:50:15 +0800

    Delete ConsoleHeader.h

--- ConsoleHeader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ConsoleObject.h"
-#include "ConsoleButton.h"
-#include "ConsoleText.h"
-#include "ConsoleScene.h"
-#include "ConsoleCharacter.h"
-#include "ConsoleImage.h"
-using namespace ConsoleController;
